e078eb68285e29e8e3114235ab849dee3d68bd23
Maycon <m.vinnicyus@gmail.com>
2020-07-29 19:30:52 -0300

    using gulp series, copy and clean

--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,21 @@
 const gulp = require('gulp');
 const imagemin = require('gulp-imagemin');
+const clean = require('gulp-clean');
 
-gulp.task('build-img', () => {
-  gulp.src('src/img/**/*').pipe(imagemin()).pipe(gulp.dest('src/img'));
+gulp.task('clean', () => {
+  return gulp.src('src').pipe(clean());
 });
+
+gulp.task(
+  'copy',
+  gulp.series('clean', () => {
+    return gulp.src('dist/**/*').pipe(gulp.dest('src'));
+  })
+);
+
+gulp.task(
+  'build-img',
+  gulp.series('copy', () => {
+    return gulp.src('src/img/**/*').pipe(imagemin()).pipe(gulp.dest('src/img'));
+  })
+);
